ebac79b594a37a08a72fe7cc9901e1fbf16b0a01
Oriol <150459853+Oriol65@users.noreply.github.com>
2025-11-16 05:06:34 +0000

    Basic product working + Placeholder btn

--- javascript.js
+++ javascript.js
@@ -1,20 +1,29 @@
 //Create a webpage with a 16x16 grid of square divs. [ ]
-let size=16; // Using let to be able to modify it in the future if we want to change the size/resolution
 const gridSpace = document.querySelector(".content"); //Selects the content class in the document
+const buttons = document.querySelector(".buttons");
+const reset = document.createElement('button')
+reset.classList.add('reset')
+reset.textContent='Reset?';
+reset.addEventListener('click',(e)=>{
+    console.log('reset');
+})
+buttons.appendChild(reset)
 
-let array= [];
+function createGrid(size){
+    
+    for (let i = 0; i<size; i++){ //first loop for rows, need to append columns created on the bottom
+        const row = document.createElement("div");
+        row.classList.add("row");
 
-for (let i = 0; i<size; i++){ //first loop for rows, need to append columns created on the bottom
-    let subarray=[];
-    const row = document.createElement("div");
-    row.classList.add("row");
+        for (let j = 0; j<size; j++){ //second loop for columns, need to create all 16 divs
+            const column = document.createElement("div");
+            column.classList.add("box");
+            column.style.backgroundColor="white";
+            column.addEventListener("mouseover",()=>{column.style.backgroundColor="black"})
+            row.appendChild(column);
+        }
+        gridSpace.appendChild(row);
 
-    for (let j = 0; j<size; j++){ //second loop for columns, need to create all 16 divs
-        const column = document.createElement("div");
-        column.classList.add("box");
-        row.appendChild(column);
-        subarray.push([i, j])
     }
-    gridSpace.appendChild(row);
-    array.push(subarray);
 }
+createGrid(16);
